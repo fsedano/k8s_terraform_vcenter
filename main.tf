45271e2adf5dd3c8865f62676d73998d142ccd28
Francisco Sedano <fran@fransedano.net>
2020-06-25 18:28:16 +0200

    Add vsphere configuration

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "vsphere_virtual_machine" "vm" {
   num_cpus = 2
   memory   = 8192
   guest_id = "ubuntu64Guest"
-  folder = "Kube-test"
+  folder = "Kube-test3"
 
 connection {
     type     = "ssh"
@@ -150,8 +150,6 @@ clone {
 
       network_interface {
       }
-
-      //ipv4_gateway = "192.168.30.254"
     }
 }
 
@@ -165,154 +163,3 @@ clone {
   }
 }
 
-/*
-//https://computingforgeeks.com/how-to-deploy-ceph-storage-cluster-on-ubuntu-18-04-lts/
-resource "vsphere_virtual_machine" "mon" {
-  name             = "kube16-ceph-mon${count.index}"
-  count = 3
-  nested_hv_enabled = true
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 8192
-  guest_id = "ubuntu64Guest"
-
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    linked_clone = true
-    customize {
-      linux_options {
-        host_name = "kube16-ceph-mon${count.index}"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-      }
-
-    }
-}
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 32
-  }
-}
-
-resource "vsphere_virtual_machine" "osd" {
-  name             = "kube16-ceph-osd${count.index}"
-  count = 3
-  nested_hv_enabled = true
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 8192
-  guest_id = "ubuntu64Guest"
-
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    linked_clone = true
-    customize {
-      linux_options {
-        host_name = "kube16-ceph-osd${count.index}"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-      }
-
-    }
-}
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 32
-  }
-}
-
-resource "vsphere_virtual_machine" "rgw" {
-  name             = "kube16-ceph-rgw"
-  nested_hv_enabled = true
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 8192
-  guest_id = "ubuntu64Guest"
-
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    linked_clone = true
-    customize {
-      linux_options {
-        host_name = "kube16-ceph-rgw"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-      }
-
-    }
-}
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 32
-  }
-}
-resource "vsphere_virtual_machine" "admin" {
-  name             = "kube16-ceph-admin"
-  nested_hv_enabled = true
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 8192
-  guest_id = "ubuntu64Guest"
-
-
-clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    linked_clone = true
-    customize {
-      linux_options {
-        host_name = "kube16-ceph-admin"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-      }
-
-    }
-}
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 32
-  }
-}
-
-output "ip_address" {
-  value = vsphere_virtual_machine.vm[0].default_ip_address
-}
-*/
